a01b19e5115d5f51cb8180cd609013a3e7d81723
fxxrytail <42585714+fxxrytail@users.noreply.github.com>
2019-06-09 02:31:00 +0800

    Add files via upload

---
+++ test/javascript.js
@@ -0,0 +1,35 @@
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("btnTop").style.display = "block";
+    } else {
+        document.getElementById("btnTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function openSurvey() {
+	btnSurveyText = document.getElementById("btnSurvey").innerHTML;
+	
+	if (btnSurveyText != "Click to hide"){
+		document.getElementById("about").style.display = "none";
+		document.getElementById("survey").style.display = "block";
+		document.getElementById("box").style.border = "2px solid #797039";
+		btnSurveyText = document.getElementById("btnSurvey").innerHTML = "Click to hide";
+	}
+	
+	else {
+		document.getElementById("about").style.display = "block";
+		document.getElementById("survey").style.display = "none";
+		document.getElementById("box").style.border = "block";
+		btnSurveyText = document.getElementById("btnSurvey").innerHTML = "Click this if you're new to Stray Kids ^^";
+	}
+}
