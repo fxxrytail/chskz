fc1971fd22d58cf2332acd4cba0bb0d743d6d88c
fxxrytail <42585714+fxxrytail@users.noreply.github.com>
2019-08-18 03:59:05 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -47,33 +47,24 @@ function closeModal() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-function openCity(evt, cityName) {
+function openTab(evt, tabName, tc, tb) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName(tc);
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName(tb);
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
